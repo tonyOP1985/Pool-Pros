3947d709904f274adfbd51331f3bcfbc538a8ff6
tonyOP1985 <toliverpaull@gmail.com>
2018-03-25 10:15:54 -0400

    cross browser compatiblitly

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -47,7 +47,7 @@
           </div>
           <div class="nav__menu">
             <ul class="nav__menu-items">
-              <li class="nav__menu-item" v-for="(menuLink, index) in menuLinks" :key="index">
+              <li class="nav__menu-item" aria-haspopup="true" v-for="(menuLink, index) in menuLinks" :key="index">
                 <a href="#" :title="menuLink.menuTitle">
                   {{ menuLink.menuTitle }}
                 </a>
@@ -360,8 +360,22 @@ export default {
       }
     }
   }
-
+// .nav__menu-item:focus, .nav__menu-item:active
   .nav__menu-item:hover .nav__submenu {
+    display: block;
+    opacity: 1;
+    z-index: 1;
+    transition-delay: all 0s, 0s, 0.3s;
+  }
+
+  .nav__menu-item:focus .nav__submenu {
+    display: block;
+    opacity: 1;
+    z-index: 1;
+    transition-delay: all 0s, 0s, 0.3s;
+  }
+
+  .nav__menu-item:active .nav__submenu {
     display: block;
     opacity: 1;
     z-index: 1;
